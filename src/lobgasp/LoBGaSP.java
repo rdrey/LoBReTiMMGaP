8178c844d837eaaaf81dd40a68781acb9550c855
Rainer Dreyer <rdrey1@gmail.com>
2011-05-27 16:01:54 +0200

    init - spatial server

---
+++ src/lobgasp/LoBGaSP.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lobgasp;
+
+/**
+ *
+ * @author rainerdreyer
+ */
+public class LoBGaSP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
