dcdee5648361ef08edc834f6bdfe1f70cc373f28
Lawrence Webley <lwebley@gmail.com>
2011-08-02 13:19:16 +0200

    Added initial commit for the network server.
    Signed-off-by: Lawrence Webley <lwebley@gmail.com>

---
+++ Network Component/NetworkServer/src/networkserver/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package networkserver;
+
+/**
+ *
+ * @author Lawrence
+ */
+public class Main
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+{
+       
+    }
+
+}
