f28ffbe2bd9fcfcaa003ecaccdc2a9870cef8dae
Lawrence Webley <lwebley@gmail.com>
2011-08-11 12:39:02 +0200

    Create an implementation thread for ServerDaemonThread. Testing to see how git handles multiple branches.
    Signed-off-by: Lawrence Webley <lwebley@gmail.com>

---
+++ Network Component/NetworkServer/src/networkserver/Threads/CustomDaemonThread.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package networkserver.Threads;
+
+/**
+ * @date 2011/08/11
+ * @author Lawrence Webley
+ */
+public class CustomDaemonThread {
+
+}
