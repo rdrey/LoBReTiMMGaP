cbea551d456275d03315d76a4157ba65b8507381
Lawrence Webley <lwebley@gmail.com>
2011-08-22 10:53:07 +0200

    Converting to service

---
+++ Network Component/NetworkClient/src/com/Lobretimgap/NetworkClient/NetworkService.java
@@ -0,0 +1,15 @@
+package com.Lobretimgap.NetworkClient;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class NetworkService extends Service{
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
