770d1ab2cb83add7e1a2b0eb719b6f057148722a
Rizmari Versfeld <rizziepit@gmail.com>
2011-08-28 17:28:46 +0200

    added network implementation

---
+++ Lokemon/src/com/Lobretimgap/NetworkClient/Lokemon/CoreNetworkThreadLokemon.java
@@ -0,0 +1,23 @@
+package com.Lobretimgap.NetworkClient.Lokemon;
+
+import networkTransferObjects.NetworkMessage;
+import networkTransferObjects.PlayerRegistrationMessage;
+import android.lokemon.Trainer;
+import android.util.Log;
+
+import com.Lobretimgap.NetworkClient.NetworkVariables;
+import com.Lobretimgap.NetworkClient.Threads.CoreNetworkThread;
+
+public class CoreNetworkThreadLokemon extends CoreNetworkThread {
+
+	@Override
+	public PlayerRegistrationMessage getPlayerRegistrationInformation() {		
+		return new PlayerRegistrationMessage(Trainer.player.nickname);
+	}
+
+	@Override
+	public void processInitialGameState(NetworkMessage message) {
+		Log.d(NetworkVariables.TAG, "Received initial game state. Message: "+message.getMessage());
+	}
+
+}
