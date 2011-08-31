627f14c31dd1e65fd335b6139fcee9c1b3427205
Rizmari Versfeld <rizziepit@gmail.com>
2011-08-31 05:00:50 +0200

    a few missing resources added

---
+++ Lokemon/src/android/lokemon/BattleScreen.java
@@ -0,0 +1,12 @@
+package android.lokemon;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BattleScreen extends Activity {
+	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        setContentView(R.layout.battle);
+	}
+}
