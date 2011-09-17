3f01649ca11aed3096aab440d3261ee629563f87
Lawrence Webley <lwebley@gmail.com>
2011-09-17 13:42:30 +0200

    After much testing and breaking and fixing things, I think this ordering for the timestamp might work.
    Signed-off-by: Lawrence Webley <lwebley@gmail.com>

--- Network Component/NetworkClient/src/networkTransferObjects/NetworkMessage.java
+++ Network Component/NetworkClient/src/networkTransferObjects/NetworkMessage.java
@@ -1,18 +1,16 @@
-
 package networkTransferObjects;
-
 
 /**
  * Used to pass information between the client and the server.
  * @date 2011/08/02
  * @author Lawrence Webley
  */
-public class NetworkMessage
-{
+public class NetworkMessage {
 
     //Used internally for network message classification.
     public enum MessageType //Comments show where the type can be received
     {
+
         UPDATE_MESSAGE, //Client and Server
         REQUEST_MESSAGE,//Client and Server
         INITIAL_GAME_STATE_MESSAGE, //Client only
@@ -25,51 +23,41 @@ public class NetworkMessage
         LATENCY_REQUEST_MESSAGE, //Server & Client.
         LATENCY_RESPONSE_MESSAGE, //Server & Client
         DIRECT_COMMUNICATION_MESSAGE //client to client (router through server sometimes)
-
     }
-
-    private long timeStamp;
-	private MessageType messageType;
+    private MessageType messageType;
     private String message;
-    
+    private long timeStamp;
 
-    public NetworkMessage(String message)
-    {
-    	setMessage(message);       
+    public NetworkMessage(String message) {
+        setMessage(message);
     }
 
-    public NetworkMessage()
-    {
-    	message = "";    	
+    public NetworkMessage() {
+        message = "";
     }
 
-    public String getMessage()
-    {
+    public String getMessage() {
         return message;
     }
 
-    public void setMessage(String message)
-    {
-    	this.message = message;
+    public void setMessage(String message) {
+        this.message = message;
     }
 
     //Used internally for network message classification. Don't use this.
-    public void setMessageType(MessageType mType)
-    {
+    public void setMessageType(MessageType mType) {
         messageType = mType;
     }
 
-    public MessageType getMessageType()
-    {
+    public MessageType getMessageType() {
         return messageType;
     }
-    
-    public long getTimeStamp() {
-		return timeStamp;
-	}
 
-	public void setTimeStamp(long timeStamp) {
-		this.timeStamp = timeStamp;
-	}
-   
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
 }
