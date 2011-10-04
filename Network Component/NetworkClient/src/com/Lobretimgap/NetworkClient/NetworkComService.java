49795914687d17b097eea68a9cba4708aa481064
Lawrence Webley <lwebley@gmail.com>
2011-10-04 14:45:38 +0200

    Logs now being saved to sd card every 60 seconds.
    Signed-off-by: Lawrence Webley <lwebley@gmail.com>

--- Network Component/NetworkClient/src/com/Lobretimgap/NetworkClient/NetworkComService.java
+++ Network Component/NetworkClient/src/com/Lobretimgap/NetworkClient/NetworkComService.java
@@ -1,11 +1,16 @@
 package com.Lobretimgap.NetworkClient;
 
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import networkTransferObjects.NetworkMessage;
 
 import com.Lobretimgap.NetworkClient.Threads.CoreNetworkThread;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Environment;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -13,12 +18,55 @@ public class NetworkComService <T extends CoreNetworkThread> extends Service{
 
 	
 	private IBinder binder;
+	private Timer logSaver;
 	
 	public void onCreate()
 	{	
-		try {			
-			
+		try {						
 			binder = new NetworkComBinder(getApplicationContext());
+			
+			logSaver.schedule(new TimerTask() {
+				
+				@Override
+				public void run() {
+					
+					try
+					{
+						
+						String filePrefix = Environment.getExternalStorageDirectory()+"/";
+						
+						String cmd = "logcat -d -v time >> "+filePrefix+"fullLog.log";					
+					    Runtime.getRuntime().exec(cmd);
+					    
+					    cmd = "logcat -d -v time -s NetworkClient >> "+filePrefix+"networkclientLog.log";
+					    Runtime.getRuntime().exec(cmd);
+					    
+					    cmd = "logcat -d -v time -s bandwidth >> "+filePrefix+"bandwidthLog.log";
+					    Runtime.getRuntime().exec(cmd);
+					    
+					    cmd = "logcat -d -v time -s compression >> "+filePrefix+"compressionLog.log";
+					    Runtime.getRuntime().exec(cmd);
+					    
+					    cmd = "logcat -d -v time -s latency >> "+filePrefix+"latencyLog.log";
+					    Runtime.getRuntime().exec(cmd);
+					    
+					    cmd = "logcat -d -v time -s networks >> "+filePrefix+"networksLog.log";
+					    Runtime.getRuntime().exec(cmd);
+					    
+					    cmd = "logcat -d -v time -s timesync >> "+filePrefix+"timesyncLog.log";
+					    Runtime.getRuntime().exec(cmd);
+					    
+					    cmd = "logcat -c";
+					    Runtime.getRuntime().exec(cmd);
+				    
+					}
+					catch(IOException e)
+					{
+						e.printStackTrace();
+					}
+				}
+			}, 1000, 60000);
+			
 		} catch (IllegalAccessException e) {
 			Log.e(NetworkVariables.TAG, "Error thrown in Network thread instantiation: " + e.getMessage(), e);			
 		} catch (InstantiationException e) {
@@ -48,6 +96,7 @@ public class NetworkComService <T extends CoreNetworkThread> extends Service{
 			}
 			
 		}
+		logSaver.cancel();
 		super.onDestroy();
 	}
 
